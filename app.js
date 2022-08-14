9b5720a536c97e831b1e94d59390abd19aaa699d
Amadi Y <absnacks17@gmail.com>
2022-08-14 11:10:22 -0500

    retrieved all the necessary elements

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const numOfPages = document.querySelector("#numOfPages");
 const submitBtn = document.querySelector("#submit-btn");
 const bookDislayGrid = document.querySelector(".books-display");
 
+// saves all the books of the user
+let booksArray = [];
+
+//function that retrieves the user book choice
+
+// function that displays the users book and saves the information into the booksArray
+
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault;
-    console.log(title.nodeValue);
-    console.log(author.nodeValue);
-    console.log(numOfPages.nodeValue);
+    console.log(title.value);
+    console.log(author.value);
+    console.log(numOfPages.value);
 })
