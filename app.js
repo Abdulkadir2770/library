082152080a7949e31a563a88718961489c2beef1
Amadi Y <absnacks17@gmail.com>
2022-08-14 12:08:08 -0500

    no big changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const title = document.querySelector("#title").nodeValue;
+const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const numOfPages = document.querySelector("#numOfPages");
 const submitBtn = document.querySelector("#submit-btn");
@@ -8,11 +8,20 @@ const bookDislayGrid = document.querySelector(".books-display");
 let booksArray = [];
 
 //function that retrieves the user book choice
+const bookInfo = () => {
+
+}
 
 // function that displays the users book and saves the information into the booksArray
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault;
+    
+    const book = document.createElement("div");
+    bookDislayGrid.appendChild(book);
+    book.classList.add("book");
+    book.innerText = "It works";
+
     console.log(title.value);
     console.log(author.value);
     console.log(numOfPages.value);
