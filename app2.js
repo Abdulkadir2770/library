717cd25f5a9d4e504141be0a3f63bf55f9af487d
Amadi Y <absnacks17@gmail.com>
2022-08-15 08:25:58 -0500

    final

---
+++ app2.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, numOfPages) {
+  // the constructor...
+    this.author = author;
+    this.title = title;
+    this.numOfPages = numOfPages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
