b25ca27c817cb64777c842a743bee216c4f1a9b2
Amadi Y <absnacks17@gmail.com>
2022-08-16 06:47:02 -0500

    no major changes

--- app.js
+++ app.js
@@ -4,24 +4,15 @@ const numOfPages = document.querySelector("#numOfPages");
 const submitBtn = document.querySelector("#submit-btn");
 const bookDislayGrid = document.querySelector(".books-display");
 
-// saves all the books of the user
-let booksArray = [];
-
-//function that retrieves the user book choice
-const bookInfo = () => {
-
-}
-
 // function that displays the users book and saves the information into the booksArray
-
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault;
-    
+
     // creating a <div> element and appending it into the DOM
     const bookDivTag = document.createElement("div");
     bookDislayGrid.appendChild(bookDivTag);
     bookDivTag.classList.add("book");
-
+    
     // create the <p>'s and appending it
     const p1 = document.createElement("p");
     const p2 = document.createElement("p");
@@ -35,7 +26,24 @@ submitBtn.addEventListener("click", (e) => {
     p2.innerText = `Author: ${author.value}`;
     p3.innerText = `Num of Pages: ${numOfPages.value}`;
 
+    // creating a removal button
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove");
+    removeButton.innerText = "Remove";
+    bookDivTag.appendChild(removeButton);
+    return removeButton;
+
     title.value = "Title";
     author.value = "Author";
     numOfPages.value = 0;
+
+    
 })
+
+function remove() {
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove");
+    removeButton.innerText = "Remove";
+    bookDivTag.appendChild(removeButton);
+
+}
